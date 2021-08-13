7e15199f8e380e02c5ecac6dcd13d47d817d5322
Lutful Kabir <lutfulkbr@yahoo.com>
2021-08-13 12:07:38 -0400

    commit tree a636d37237718d77840703017c45a12300929f60

--- js/createStore.js
+++ js/createStore.js
@@ -1,28 +1,51 @@
-let state;
+
+function createStore(reducer) {
+  let state;
+
+  function dispatch(action){
+    state = reducer(state, action);
+    render();
+  };
+
+  function getState() {
+    return state
+  }
+
+  return {
+    dispatch,
+    getState
+  }
+
+}
 
 function reducer(state = { count: 0 }, action) {
   switch (action.type) {
     case 'INCREASE_COUNT':
       return { count: state.count + 1 };
 
+    case 'DECREASE_COUNT':
+      return { count: state.count - 1 };
+
     default:
       return state;
   }
 };
 
-function dispatch(action){
-  state = reducer(state, action);
-  render();
-};
-
 function render() {
   let container = document.getElementById('container');
-  container.textContent = state.count;
+  container.textContent = store.getState().count;
 };
 
-dispatch({ type: '@@INIT' })
-let button = document.getElementById('button');
+let store = createStore(reducer)
+store.dispatch({type: '@@INIT'})
 
-button.addEventListener('click', function() {
-    dispatch({ type: 'INCREASE_COUNT' });
+let button = document.getElementById('button');
+let button1 = document.getElementById('button1');
+
+button.addEventListener('click', () => {
+    store.dispatch({ type: 'INCREASE_COUNT' });
+})
+
+button1.addEventListener('click', () => {
+  store.dispatch({ type: 'DECREASE_COUNT' });
 })
